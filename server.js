e7424ce488d2d855a1eb258a608ab8f8b851f6c5
Kartik Gupta <00guptakartik2006@gmail.com>
2025-03-08 17:20:10 +0530

    first file push for backend

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
+
+// Create HTTP server
 const server = http.createServer(app);
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+module.exports = server;
